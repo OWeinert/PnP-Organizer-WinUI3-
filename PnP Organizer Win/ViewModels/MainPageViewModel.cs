9c8dd3c939ffef5f843d04300625689447d86c21
Piggo41 <mr.piggo123@gmail.com>
2023-04-06 02:39:15 +0200

    added IViewModel inheritance

--- PnP Organizer Win/ViewModels/MainPageViewModel.cs
+++ PnP Organizer Win/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,23 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using PnPOrganizer.ViewModels.Interfaces;
+using System;
 
 namespace PnPOrganizer.ViewModels
 {
-    public partial class MainPageViewModel : ObservableObject
+    public partial class MainPageViewModel : ObservableObject, IViewModel
     {
+        public bool IsInitialized => throw new NotImplementedException();
+
+        public event EventHandler? Initialized;
+
+        public MainPageViewModel() 
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
