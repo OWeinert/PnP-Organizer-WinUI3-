c6557e0f1821bebc39c61af03c3eb28cc99db651
Piggo41 <mr.piggo123@gmail.com>
2023-04-08 17:59:59 +0200

    Added IView and IViewFor<> interfaces

---
+++ PnP Organizer Win/Views/Interfaces/IViewFor.cs
@@ -0,0 +1,9 @@
+﻿using PnPOrganizer.ViewModels.Interfaces;
+
+namespace PnPOrganizer.Views.Interfaces
+{
+    public interface IViewFor<TViewModel> : IView where TViewModel : IViewModel
+    {
+        public TViewModel ViewModel { get; }
+    }
+}
