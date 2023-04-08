a93d4152ec8a2f1b170f82864dfc85d3a759fe69
Piggo41 <mr.piggo123@gmail.com>
2023-04-08 18:00:38 +0200

    added CharacterPage

---
+++ PnP Organizer Win/Views/CharacterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using PnPOrganizer.ViewModels;
+using PnPOrganizer.Views.Interfaces;
+
+namespace PnPOrganizer.Views
+{
+    public sealed partial class CharacterPage : Page, IViewFor<CharacterPageViewModel>
+    {
+        public CharacterPageViewModel ViewModel { get; }
+
+        public CharacterPage()
+        {
+            ViewModel = Ioc.Default.GetRequiredService<CharacterPageViewModel>();
+            InitializeComponent();
+        }
+    }
+}
