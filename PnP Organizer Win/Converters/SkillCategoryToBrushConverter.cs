f77eacd9883d8a8aaf3064c62446568199b4ab96
Piggo41 <mr.piggo123@gmail.com>
2023-04-08 17:54:29 +0200

    Added SkillCategoryToBrushConverter and SkillCategoryToColorConverter

---
+++ PnP Organizer Win/Converters/SkillCategoryToBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.WinUI.UI.Controls.ColorPickerConverters;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using PnPOrganizer.Core;
+using PnPOrganizer.Core.Character;
+using PnPOrganizer.Core.Character.SkillSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI;
+
+namespace PnPOrganizer.Converters
+{
+    public class SkillCategoryToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not SkillCategory)
+                throw new ArgumentException("value type is not SkillCategory!", nameof(value));
+
+            return new SolidColorBrush((Color)new SkillCategoryToColorConverter().Convert(value, targetType, parameter, language));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PnP Organizer Win/Converters/SkillCategoryToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.WinUI.UI.Controls.ColorPickerConverters;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using PnPOrganizer.Core;
+using PnPOrganizer.Core.Character;
+using PnPOrganizer.Core.Character.SkillSystem;
+using System;
+namespace PnPOrganizer.Converters
+{
+    public class SkillCategoryToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not SkillCategory)
+                throw new ArgumentException("value type is not SkillCategory!", nameof(value));
+
+            return (SkillCategory)value switch
+            {
+                SkillCategory.Melee => Utils.ColorFromArgbValue(ISkillsService.MeleeSkillColor),
+                SkillCategory.Ranged => Utils.ColorFromArgbValue(ISkillsService.RangedSkillColor),
+                _ => Utils.ColorFromArgbValue(ISkillsService.CharSkillColor),
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
