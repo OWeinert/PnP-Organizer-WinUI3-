24561eae3456f6511abe640e4306f04c78ece6b5
Piggo41 <mr.piggo123@gmail.com>
2023-04-08 18:00:28 +0200

    added CharacterPageViewModel

---
+++ PnP Organizer Win/ViewModels/CharacterPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PnPOrganizer.ViewModels.Interfaces;
+using PnPOrganizer.Views;
+using PnPOrganizer.Views.Interfaces;
+using System;
+
+namespace PnPOrganizer.ViewModels
+{
+    public class CharacterPageViewModel : ObservableObject, IViewModel
+    {
+        private bool _isInitialized = false;
+        internal static object MaxEnergyModifierBonus;
+
+        public bool IsInitialized => _isInitialized;
+
+        public static object MaxHealthModifierBonus { get; internal set; }
+        public static object MaxStaminaModifierBonus { get; internal set; }
+        public static object InitiativeModifierBonus { get; internal set; }
+
+        public event EventHandler? Initialized;
+
+        public CharacterPageViewModel()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
