b721681b37ecfd18bf17ca0ac11a13a4d9119bd1
Piggo41 <mr.piggo123@gmail.com>
2023-04-08 18:04:55 +0200

    readded StatModifiers

---
+++ PnP Organizer Win/Core/Character/StatModifiers/CalculatorStatModifier.cs
@@ -0,0 +1,28 @@
+﻿using PnPOrganizer.Core.BattleAssistant;
+using PnPOrganizer.Core.Character.SkillSystem;
+
+namespace PnPOrganizer.Core.Character.StatModifiers
+{
+    public struct CalculatorStatModifier : IStatModifier
+    {
+        public CalculatorValueType CalculatorValueType { get; private set; }
+        public ApplianceMode ApplianceMode { get; private set; }
+        public Dice Dice { get; private set; }
+        public double Bonus { get; private set; }
+        public CalculatorBonusType CalculatorBonusType { get; private set; }
+
+        public CalculatorStatModifier(CalculatorValueType calculatorValueType, ApplianceMode applianceMode, double bonus,
+            CalculatorBonusType calculatorBonusType = CalculatorBonusType.Additive)
+            : this(calculatorValueType, applianceMode, Dice.D1, bonus, calculatorBonusType) { }
+
+        public CalculatorStatModifier(CalculatorValueType calculatorValueType, ApplianceMode applianceMode, Dice dice, double bonus = 0, 
+            CalculatorBonusType calculatorBonusType = CalculatorBonusType.Additive)
+        {
+            CalculatorValueType = calculatorValueType;
+            ApplianceMode = applianceMode;
+            Dice = dice;
+            Bonus = bonus;
+            CalculatorBonusType = calculatorBonusType;
+        }
+    }
+}
---
+++ PnP Organizer Win/Core/Character/StatModifiers/CharacterStatModifier.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace PnPOrganizer.Core.Character.StatModifiers
+{
+    public struct CharacterStatModifier : IStatModifier
+    {
+        public PropertyInfo StatPropertyInfo { get; private set; }
+        public int Bonus { get; private set; }
+
+        public CharacterStatModifier(string statPropertyName, int bonus) 
+        {
+            // TODO
+            //StatPropertyInfo = typeof(OverviewViewModel).GetProperty(statPropertyName)!;
+            Bonus = bonus;
+        }
+    }
+}
