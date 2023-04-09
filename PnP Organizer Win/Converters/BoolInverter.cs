4235a527b695a70c75b22d54286a23c87b448841
Piggo41 <mr.piggo123@gmail.com>
2023-04-09 19:32:29 +0200

    added BoolInverter

---
+++ PnP Organizer Win/Converters/BoolInverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace PnPOrganizer.Converters
+{
+    public class BoolInverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => Invert(value);
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => Invert(value);
+
+        private bool Invert(object value)
+        {
+            if (value is not bool)
+                throw new ArgumentException("Value type is not bool!", nameof(value));
+
+            return !(bool)value;
+        }
+    }
+}
