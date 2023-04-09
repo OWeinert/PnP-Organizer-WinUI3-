caa77d486814797d66c37ce51e9059a1d7c7ed65
Piggo41 <mr.piggo123@gmail.com>
2023-04-09 19:55:15 +0200

    removed unnecessary usings

--- PnP Organizer Win/Views/RulesPage.xaml.cs
+++ PnP Organizer Win/Views/RulesPage.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Hosting;
-using Microsoft.UI.Xaml.Media;
 using PnPOrganizer.Core;
 using PnPOrganizer.ViewModels;
 using PnPOrganizer.Views.Interfaces;
-using System;
-using System.Diagnostics;
-using Windows.UI.WindowManagement;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace PnPOrganizer.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class RulesPage : Page, IViewFor<RulesPageViewModel>
     {
         public RulesPageViewModel ViewModel { get; }
@@ -33,7 +22,6 @@ namespace PnPOrganizer.Views
 
         private void OpenWindowButton_Click(object sender, RoutedEventArgs e)
         {
-            
             var externalWindow = WindowHelper.CreateWindow();
             var rootPage = new ExternalRulesPage();
             externalWindow.Content = rootPage;
